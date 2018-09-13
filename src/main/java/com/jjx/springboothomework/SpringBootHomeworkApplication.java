6ab77a793551ef836904c280adc316cefa6296ba
jiajingxin <jiajingxin@foxmail.com>
2018-09-13 13:45:37 +0800

    first Commit

---
+++ src/main/java/com/jjx/springboothomework/SpringBootHomeworkApplication.java
@@ -0,0 +1,12 @@
+package com.jjx.springboothomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHomeworkApplication.class, args);
+	}
+}
